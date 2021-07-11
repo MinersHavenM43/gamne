0e53882494829d327ec0187feca6146d54c16142
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-11 20:44:24 -0300

    Update main.cpp
    gpl

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+// main file
+// Copyright (C) 2021 MinersHavenM43, see 'h.h' for details
 #include "h.h"
 using namespace std;
 
 // int a = 2; test variable
 int main() {
+    std::cout <<
+    "Gamne by MinersHaven\n"
+    "Released under GNU GPL version 2, and thus does not come with any warranty. See COPYING for details"
+    << endl;
     std::cout << "Press any key to start to start." << endl;
     game::move();
     return 0;
