5be9bf8aea03239c48e056d195afdc15a582a3b3
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-14 13:58:36 -0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -5,18 +5,17 @@
 using namespace std;
 
 namespace game {
-    int player[2] = {10,0};
-    unsigned int grand(int x, int y, int k, int seed = seed) {
-        srand(x+seed);
-        int xr = rand();
-        srand(y+seed);
-        int yr = rand();
-        return (xr + yr) % k;
+    short player[2] = {0,0};
+    vector<coord> emptytiles;
+
+    int grand(short x, short y, int k, int seed = seed) {
+        srand(13*x + 29*y);
+        return rand() % k;
     }
     string getChar(int x, int y) {
         if(x == player[0] && y == player[1]) return "@";
-        if(content::wall(x,y,TYPE::glyph) == ".") return content::biome(x,y,TYPE::glyph);
-        return content::wall(x,y,TYPE::glyph);
+        if(!content::wall(x,y).id) return content::biome(x,y).glyph;
+        return content::wall(x,y).glyph;
     }
     void make() { // renders to console
         int x = player[0] - prange; int y = player[1] + prange;
