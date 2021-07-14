f4af89624147b8a67df1a623d0b675fcf6daa2a7
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-14 13:58:39 -0300

    Update content.cpp

--- content.cpp
+++ content.cpp
@@ -9,35 +9,48 @@ namespace content {
     int testf(int h) {
         return h + 3;
     } */
+
     float distance(int x, int y) {
         return sqrt(x*x + y*y);
     }
-    string wallrh(string glyph, string name, int pass, int type) { // wall return help
-        if(type == TYPE::glyph) return glyph;
-        else if(type == TYPE::name) return name;
-        else if(type == TYPE::pass) return to_string(pass);
+    bool isEmpty(coord c) {
+        int ecsize = game::emptytiles.size();
+        int z = 0; // doesn't work if it's just int z; for some reason
+        while(z < ecsize) {
+            if(game::emptytiles[z] == c) return true;
+            z++;
+        }
+        return false;
     }
-    string biome(int x, int y, int type) {
+    coord xy(int x, int y) {
+        return {x: (short)x, y: (short)y};
+    }
+
+    Biome biome(int x, int y) {
         if(sinf(x/10.) + sinf(y/10.) > 1) {
-            return type ? "Mountains" : "^";
+            return {2, "^", "Mountains"};
         }
         if(sinf(x/10.) + sinf(y/10.) > -0.5) {
-            return type ? "Forest" : ",";
+            return {1, ",", "Forest"};
         }
-        return type ? "Plains" : "."; // default
+        return {0, ".", "Plains"}; // default
     }
-    string wall(int x, int y, int type) {
-        string cbiome = biome(x, y, TYPE::name);
-        if(cbiome == "Forest") {
-            if(game::grand(x, y, 20, seed) < 1) {
-                return wallrh("+", "tree", PASSTYPE::nopass, type);
-            }
+    Wall wall(int x, int y) {
+        int cbiome = biome(x, y).id;
+        if(isEmpty(xy(x,y))) return {0, "", "", PASSTYPE::pass};
+        if(x == 0 && y == 0) return {0, "", "", PASSTYPE::pass}; // no walls at spawn :)
+        switch(cbiome) {
+            case 1:
+                if(game::grand(x, y, 20, seed) < 4) {
+                    return {1, "+", "tree", PASSTYPE::chop};
+                }
+            break;
+            case 2:
+                if(game::grand(x, y, 20, seed) < 10) {
+                    return {2, "Λ", "mountain", PASSTYPE::nopass};
+                }
+            break;
         }
-        if(cbiome == "Mountains") {
-            if(game::grand(x, y, 20, seed) < 19) {
-                return wallrh("Λ", "mountain", PASSTYPE::nopass, type);
-            }
-        }
-        return "."; // keep this at last
+        return {0, "", "", PASSTYPE::pass}; // keep this at last
     }
 }
