a2f687958727f6dd50c5472d63693e1bc37f734d
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-14 13:58:41 -0300

    Update h.cpp

--- h.cpp
+++ h.cpp
@@ -1,13 +1,22 @@
 // defines the variables in h.h, hopefully that will be the solution to the "multiple definitions of 'x'" thing
 // Copyright (C) 2021 MinersHavenM43, see 'h.h' for details
+
 #include "h.h"
 
 int seed = 10; // seed
 int prange = 10; // player sight range
 
+// no match for ‘operator==’ (operand types are ‘__gnu_cxx::__alloc_traits<std::allocator<coord> >::value_type {aka coord}’ and ‘coord’)
+bool operator == (const coord c1, const coord c2) {
+    if(c1.x == c2.x && c1.y == c2.y)
+        return true;
+    return false;
+}
+
 namespace PASSTYPE {
-    int pass = 0;
-    int nopass = 1;
+    int nopass = 0;
+    int pass = 1;
+    int chop = 2;
 }
 namespace TYPE {
     int glyph = 0;
