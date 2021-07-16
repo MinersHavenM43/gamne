883050bc7af04d5c78d094e016d349d3c90bdbd0
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-16 02:24:02 -0300

    Update h.cpp
    1.1

--- h.cpp
+++ h.cpp
@@ -4,10 +4,9 @@
 #include "h.h"
 
 int seed = 10; // seed
-int prange = 10; // player sight range
 
-// no match for ‘operator==’ (operand types are ‘__gnu_cxx::__alloc_traits<std::allocator<coord> >::value_type {aka coord}’ and ‘coord’)
-bool operator == (const coord c1, const coord c2) {
+// no match for ‘operator==’ (operand types are ‘__gnu_cxx::__alloc_traits<std::allocator<tile> >::value_type {aka tile}’ and ‘tile’)
+bool operator == (const tile c1, const tile c2) {
     if(c1.x == c2.x && c1.y == c2.y)
         return true;
     return false;
