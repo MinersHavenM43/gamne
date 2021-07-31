08a59131413392ca2932fd43a22e8b27a4f287b9
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-31 02:01:55 -0300

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -1,16 +1,17 @@
 // Deals with character movement
 // Copyright (C) 2021 MinersHavenM43, see 'h.h' for details
+
 #include "h.h"
 using namespace std;
 
 namespace game {
     bool noclip;
-    int passable(int x,int y) {
+    int passable(pyr x,pyr y) {
         return content::wall(x, y).pass;
     }
     void noPassM(string s, int passtype) {
-        if(passtype == PASSTYPE::nopass) make("You cannot move through the " + s + "!");
-        if(passtype == PASSTYPE::chop) make("You chop the " + s + ".");
+        if(passtype == PASS::nopass) make("You cannot move through the " + s + "!");
+        if(passtype == PASS::chop) make("You chop the " + s + ".");
     }
     void helpText() {
         std::cout <<
@@ -20,13 +21,14 @@ namespace game {
         "wasd: moves the character through the world (the @) \n"
         "3: shows coordinates \n"
         "N: noclip \n"
+        "T: debug temperature values \n"
         ;
     }
     void move() {
         while(true) {
             char k;
             std::cin >> k;
-            int oldPlayer[2] = {player[0], player[1]};
+            pyr oldPlayer[2] = {player[0], player[1]};
             switch(k) {
                 case 'w':
                     player[1]++;
@@ -53,17 +55,21 @@ namespace game {
                     noclip = !noclip;
                     std::cout << ( noclip ? "Noclip activated \n" : "Noclip deactivated \n" );
                 break;
+                case 'T': // nocliṕ
+                    std::cout <<
+                    "Height: " << Perlin::HeightMap(player[0],player[1]) <<
+                    "Temp: " << Perlin::TempMap(player[0],player[1]);;
+                break;
             }
             content::Wall pwall = content::wall(player[0],player[1]);
-            if(pwall.pass == PASSTYPE::nopass && !noclip) {
+            if(pwall.pass == PASS::nopass && !noclip) {
                 player[0] = oldPlayer[0]; player[1] = oldPlayer[1]; 
-                noPassM(pwall.name, PASSTYPE::nopass);
-                
+                noPassM(pwall.name, PASS::nopass);
             }
-            else if(pwall.pass == PASSTYPE::chop && !noclip) {
+            else if(pwall.pass == PASS::chop && !noclip) {
                 emtiles.push_back({player[0],player[1]});
                 player[0] = oldPlayer[0]; player[1] = oldPlayer[1]; 
-                noPassM(pwall.name, PASSTYPE::chop);
+                noPassM(pwall.name, PASS::chop);
             }
             else {
                 if( k == 'w' || k == 'a' || k == 's' || k == 'd' ) {
