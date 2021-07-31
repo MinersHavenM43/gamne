785014f311613aac5c57b8e1d02d0267a0a92096
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-07-31 02:01:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 // main file
 // Copyright (C) 2021 MinersHavenM43, see 'h.h' for details
+
 #include "h.h"
 using namespace std;
 
 // int a = 2; test variable
 int main() {
     std::cout <<
-    "Gamne v1.1 by MinersHaven\n"
+    "Gamne v1.2 by MinersHaven\n"
     "Released under GNU GPL version 2, and thus does not come with any warranty. See COPYING for details"
     << endl;
     std::cout << "Press 'y' to start. Press 'H' for a list of commands" << endl;
