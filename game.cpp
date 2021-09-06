12bdb5642ae9de9432788213aa8999e74b199f56
MinersHavenM43 <86806712+MinersHavenM43@users.noreply.github.com>
2021-09-05 23:21:58 -0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -5,18 +5,40 @@
 using namespace std;
 
 namespace game {
-    short player[2] = {0,0};
+    pyr player[2] = {0, 0};
     vector<tile> emtiles = { {0,0} }; // empty tiles
 
     int grand(pyr x, pyr y, int k, int seed) {
-        srand(211*x + 379*y + seed);
+        srand(x*y*y + 7*x*y + x - y + seed);
         return rand() % k;
     }
+
     string getChar(pyr x, pyr y) {
+        content::Wall wall;
+        wall = content::wall(x,y);
         if(x == player[0] && y == player[1]) return "@";
-        if(content::wall(x,y).glyph == "") return content::biome(x,y).glyph;
-        return content::wall(x,y).glyph;
+        if(wall.glyph == "") return content::biome(x,y).glyph;
+        return wall.glyph;
     }
+    SDL::Color getColor(pyr x, pyr y) {
+        content::Wall wall;
+        content::Biome biome;
+        wall = content::wall(x,y);
+        biome = content::biome(x,y);
+        if(x == player[0] && y == player[1]) return {0,0,0};
+        if(wall.color == -1) {
+            if(biome.id == 10 && wall.id == 4) return SDL::toColor(0x101040);
+            return SDL::toColor(biome.color);
+        }
+        return SDL::toColor(wall.color);
+        /*return
+        {
+            red: 255 * Perlin::TempMap(x, y),
+            green: 0,
+            blue: 255 * Perlin::HeightMap(x, y),
+        };*/
+    }
+
     string repeat(string s, int r) {
         string ss;
         for(int i; i < r; i++) {
@@ -24,21 +46,24 @@ namespace game {
         }
         return ss;
     }
-    void make(string msg) { // renders to console
+    void make(SDL_Renderer* renderer) { // renders to console
         pyr x = player[0] - PRANGE; pyr y = player[1] + PRANGE;
-        string tekh;
-        system("clear"); // clears the terminal
-        tekh += repeat(" ", PRANGE - content::biome(player[0], player[1]).name.size()/2) + content::biome(player[0], player[1]).name + "\n";
-        while(y >= player[1] - PRANGE) {
-            tekh += getChar(x,y);
+        pyr x0 = 0; pyr y0 = 0;
+        SDL::Color color;
+        SDL_Rect tile;
+        while(y0 <= 2 * PRANGE) {
+            color = getColor(x,y);
+            tile = SDL::pixel(x0,y0);
+            SDL_SetRenderDrawColor(renderer, color.red, getColor(x,y).green, getColor(x,y).blue, 255);
+            SDL_RenderFillRect(renderer, &tile);
             x++;
-            if(x > player[0] + PRANGE) {
+            x0++;
+            if(x0 > 2 * PRANGE) {
                 y--;
+                y0++;
                 x = player[0] - PRANGE;
-                tekh += "\n"; 
+                x0 = 0;
             }
         }
-        tekh += msg;
-        std::cout << tekh << endl;
     }
 }
